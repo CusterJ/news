4eff98c0a99533ca1604e10872f3fc4a369f2611
Max <amax0204@gmail.com>
2023-03-05 00:44:07 +0200

    pagination beauty

--- server/pagination.go
+++ server/pagination.go
@@ -10,16 +10,30 @@ func Pagination(page, total int) (pagelist []string) {
 	var take, allPages, skip int
 	take, _ = strconv.Atoi(os.Getenv("TAKE"))
 	allPages = int(math.Ceil(float64(total) / float64(take)))
-	// fmt.Printf("total/ take type is %T \n %v / %v = %v \n", total/take, total, take, total/take)
 	if page > 0 {
 		skip = (page - 1) * take
 	}
 	if skip > total {
 		skip = 0
 	}
-	for i := 1; i <= allPages; i++ {
-		pagelist = append(pagelist, strconv.Itoa(i))
+
+	if page-3 > 2 {
+		pagelist = append(pagelist, strconv.Itoa(1))
 	}
-	// fmt.Printf("total= %v, take = %v, page = %v, allPages = %v, skip = %v \n", total, take, page, allPages, skip)
+
+	for i := 1; i <= allPages; i++ {
+		if i >= page-3 && i <= page+3 {
+			pagelist = append(pagelist, strconv.Itoa(i))
+			continue
+		}
+		if i >= page-30 && i <= page+30 && i%10 == 0 {
+			pagelist = append(pagelist, strconv.Itoa(i))
+		}
+	}
+
+	if page+3 < allPages && allPages%10 != 0 {
+		pagelist = append(pagelist, strconv.Itoa(allPages))
+	}
+
 	return
 }
