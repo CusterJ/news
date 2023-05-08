992f2819c0fa8e829a3db3b79f4ad7028eff566e
Max <amax0204@gmail.com>
2023-05-08 11:09:24 +0300

    move user autc methods and coockie auth methods to usecases add base.go and user.go files for usecases

---
+++ usecases/base.go
@@ -0,0 +1,44 @@
+package usecases
+
+import (
+	"News/domain"
+	"context"
+)
+
+type UseCases struct {
+	articleRepo articleRepository
+	searchRepo  searchRepository
+	userRepo    userRepository
+}
+
+func NewUseCases(ar articleRepository, ur userRepository, sr searchRepository) *UseCases {
+	return &UseCases{
+		articleRepo: ar,
+		searchRepo:  sr,
+		userRepo:    ur,
+	}
+}
+
+// type repository interface {
+// 	articleRepository
+// 	searchRepository
+// 	userRepository
+// }
+
+type articleRepository interface {
+	GetByID(context.Context, string) (domain.Article, error)
+	GetNewsFromDB(context.Context, int, int) ([]domain.Article, error)
+	UpdateOne(domain.Article) error
+	Count(context.Context) (int64, error)
+}
+
+type searchRepository interface {
+	Search(string) ([]domain.Article, error)
+	UpdateOne(domain.Article) error
+}
+
+type userRepository interface {
+	UserSave(domain.User) error
+	UserExistsInDB(username string) (domain.User, bool)
+	UserFind(username, password string) error
+}
